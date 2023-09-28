edc81c9e4c543f50b5c7baa2f71495c2b34ad42c
brtdev <brtdev2@gmail.com>
2023-09-28 10:25:20 +0300

    Splash screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,9 @@ export default function Loading() {
   // Or a custom loading skeleton component
   return (
     <div className="flex h-full items-center justify-center">
-      <p className="animate-pulse font-serif text-4xl">V I B E</p>
+      <p className="animate-pulse font-serif text-white text-4xl">vybe2{" "}
+        <span className="text-base inline-block">by Brtdev</span>
+      </p>
     </div>
   );
 }
